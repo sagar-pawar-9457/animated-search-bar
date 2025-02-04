bd90eba128791212b4246ec42834862670938284
Sagar Pawar <163662611+sagar-pawar-9457@users.noreply.github.com>
2025-02-04 20:25:34 +0000

    project done

---
+++ app.js
@@ -0,0 +1,5 @@
+const btnEl = document.querySelector(".search-btn");
+const containerEl = document.querySelector(".search-container")
+btnEl.addEventListener("click",()=>{
+    containerEl.classList.toggle("active")
+})
